1907852d4d8277ddf6b0333f991bb78144135e60
tomhuhges <merzbear@gmail.com>
2018-01-04 17:28:28 +0000

    test routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Test from './components/test';
 import logo from './svg/under-construction.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <img className="under-construction" src={logo} alt="under construction" />
-        <p />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Route exact path="/" render={() => {
+            return (
+              <div>
+                <img className="under-construction" src={logo} alt="under construction" />
+              </div>
+            )
+          }}>
+          </Route>
+          <Route path="/test" component={Test} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
